AndreasKostler/starthilfe

commit c09b1286c31e3b8064a675bbff83508a248c6c22

Simplifiy build scripts

build.sbt
@@ -1,8 +1,6 @@
 uniform.project("starthilfe", "au.com.cba.omnia.starthilfe")
 
-val omnitoolVersion    = "1.12.0-20151021050758-700b9d0"
-val thermometerVersion = "1.3.0-20151122230202-55282c8"
-val ebenezerVersion    = "0.21.2-20160104234501-8e2043a"
+val omnitoolVersion    = "1.12.0-20160104235530-f3eb090"
 val rengineVersion     = "2.1.0"
 
 uniformDependencySettings
@@ -11,38 +9,22 @@ strictDependencySettings
 
 libraryDependencies :=
   depend.testing() ++ 
-  depend.omnia("ebenezer", ebenezerVersion, "test") ++
   depend.omnia("omnitool-core", omnitoolVersion) ++
-  depend.omnia("thermometer-hive", thermometerVersion, "test") ++
   Seq(
     "org.specs2"               %% "specs2-core"   % depend.versions.specs,
     "au.com.cba.omnia"         %% "omnitool-core" % omnitoolVersion % "test" classifier "tests",
     "org.rosuda.REngine"       % "REngine"        % rengineVersion
   )
 
-
 // R environment variables
-val rHome: String = sys.env.get("R_HOME").getOrElse("/Library/Frameworks/R.framework/Resources")
-val jriLDPath: String = s"$rHome/lib:$rHome/bin:"
-val ldLibraryPath: String = sys.env.get("LD_LIBRARY_PATH") map (_ ++ s":$jriLDPath") getOrElse(jriLDPath)
-val rPath = s"$rHome/library/rJava/jri"
-val path: String = sys.env.get("PATH") map (_ ++ s":$rPath") getOrElse(rPath)
+// TODO: Reasonable error message if R_HOME not set
+val rHome: String = sys.env.get("R_HOME").get
 
-unmanagedJars in Test <++= baseDirectory map { base =>
+unmanagedJars in Compile <++= baseDirectory map { base =>
   val baseDirectories = base +++ file(s"$rHome/library/rJava/jri")
   val customJars = (baseDirectories ** "*.jar")
   customJars.classpath
 }
-
-envVars in Test += "R_HOME" -> rHome
-envVars in Test += "LD_LIBRARY_PATH" -> ldLibraryPath
-envVars in Test += "PATH" -> path
-
-val libPath = s"${rHome}/library/rJava/jri:" + System.getProperty("Djava.library.path")
-
-javaOptions in Test += s"-Djava.library.path=$libPath"
-
-fork in Test := true
 
 updateOptions                     := updateOptions.value.withCachedResolution(true)
 publishArtifact           in Test := true

project/plugins.sbt
@@ -18,7 +18,7 @@ resolvers ++= Seq(
   "cloudera" at "https://repository.cloudera.com/artifactory/cloudera-repos/"
 )
 
-val uniformVersion = "1.4.0-20151122210042-f94f9b6"
+val uniformVersion = "1.6.0-20160104234203-3ff47bf"
 
 addSbtPlugin("au.com.cba.omnia" % "uniform-core"       % uniformVersion)
 
@@ -27,5 +27,3 @@ addSbtPlugin("au.com.cba.omnia" % "uniform-dependency" % uniformVersion)
 addSbtPlugin("au.com.cba.omnia" % "uniform-thrift"     % uniformVersion)
 
 addSbtPlugin("au.com.cba.omnia" % "uniform-assembly"   % uniformVersion)
-
-addSbtPlugin("au.com.cba.omnia" % "humbug-plugin"      % "0.6.1-20151008040202-1f0ccb9")

commit 56c195e03c12bb5eae5cc35fc38924f258c90429

Fix regression introduced in c09b1286c31e3b8064a675bbff83508a248c6c22

build.sbt
@@ -4,7 +4,6 @@ val omnitoolVersion    = "1.12.0-20160104235530-f3eb090"
 val rengineVersion     = "2.1.0"
 
 uniformDependencySettings
-uniformThriftSettings
 strictDependencySettings
 
 libraryDependencies :=
@@ -25,6 +24,12 @@ unmanagedJars in Compile <++= baseDirectory map { base =>
   val customJars = (baseDirectories ** "*.jar")
   customJars.classpath
 }
+
+val libPath = s"${rHome}/library/rJava/jri:" + System.getProperty("Djava.library.path")
+
+javaOptions in Test += s"-Djava.library.path=$libPath"
+
+fork in Test := true
 
 updateOptions                     := updateOptions.value.withCachedResolution(true)
 publishArtifact           in Test := true

project/plugins.sbt
@@ -24,6 +24,4 @@ addSbtPlugin("au.com.cba.omnia" % "uniform-core"       % uniformVersion)
 
 addSbtPlugin("au.com.cba.omnia" % "uniform-dependency" % uniformVersion)
 
-addSbtPlugin("au.com.cba.omnia" % "uniform-thrift"     % uniformVersion)
-
 addSbtPlugin("au.com.cba.omnia" % "uniform-assembly"   % uniformVersion)

src/test/scala/au/com/cba/omnia/starthilfe/RSpec.scala
@@ -9,9 +9,7 @@ import org.specs2.matcher.Matcher
 import scalaz.Scalaz._
 import scalaz.Equal
 
-import org.rosuda.REngine.{REXPInteger, REXPNull, REXP}
-
-import au.com.cba.omnia.omnitool.{Result, Ok, Error}
+import au.com.cba.omnia.omnitool.Result
 import au.com.cba.omnia.omnitool.test.OmnitoolProperties.resultantMonad
 import au.com.cba.omnia.omnitool.test.Arbitraries._
 